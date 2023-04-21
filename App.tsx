440f38b019a815b41c5f906cc11ddb6d0f9a5eab
Neerajsc <neerajscmails@gamil.com>
2023-04-21 18:01:27 +0530

    updated

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import Home from './src/HomeComponent';
+import NewsDetails from './src/NewsDetailsComponent';
 
+const Stack = createStackNavigator();
 
 import Login from './src/LoginComponent';
 
-
-
 function App(): JSX.Element {
   return (
-    <Login/>
+    <>
+    {/* <Login/> */}
+      
+      {/* <HomeComponent/> */}
+      {/* <NewsDetailsComponent/> */}
+
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} options={{headerShown:false}} />
+        <Stack.Screen name="Home" component={Home} options={{headerShown:false}} />
+        <Stack.Screen name="NewsDetails" component={NewsDetails} options={{headerShown:false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+    </>
   );
 }
 
